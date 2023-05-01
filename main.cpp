1e4cff6e50a3d6dbbc0848e2e79c3dc085bd1bf6
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-01 17:23:48 -0600

    Add const example

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    //const
+    // - A compile time contratint that an object cannot be modified
+    //example:
+    const int i = 9;
+    i = 6;
+
+    return 0;
+}
