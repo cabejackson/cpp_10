80c21fba85fc6ce51349d75d107c57ed482268b9
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-02 05:59:13 -0600

    Add variable rules

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 int main() {
     
     //variables
+        //give meaningful names
+        //cant contain special characters besides _
+        //cant start with numbers
+        //cant have spaces
+        //should be camelCase
     float annualSalary;
     cout << "Please enter your annual salary: ";
     cin >> annualSalary;
@@ -11,6 +16,9 @@ int main() {
     float monthlySalary = annualSalary / 12;
 
     cout << "Your monthly salary is " << monthlySalary << endl;
+    cout << "In 10 years you will earn " << annualSalary * 10 << endl;
+
+
 
 
     return 0;
