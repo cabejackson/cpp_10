59c3083b44f8df9d536f6f30ed852530510359e5
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-02 06:11:31 -0600

    Add dataTypes and use sizeof operator

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    //const
-    // - A compile time contratint that an object cannot be modified
-    //example:
-    const int i = 9;
-    i = 6;
+    
+    //dataTypes
+    int yearOfBirth = 1996;
+    char sex = 'f';
+    bool isOlderThan18 = true;
+    float avgGrade = 4.5;
+    double balance = 678357902.99;
+
+    cout << "Size of int is " << sizeof(int) << " bytes" << endl;
+    cout << "Int min value is " << INT_MIN << endl;
+    // -1, -2, -3, ... -2147483648
+    cout << "Int min value is " << INT_MAX << endl;
+    // 0, 1, 2, 3, ... 2147483647
+
+    cout << "Size of bool is " << sizeof(bool) << " bytes" << endl;
+    cout << "Size of char is " << sizeof(char) << " bytes" << endl;
+    cout << "Size of float is " << sizeof(float) << " bytes" << endl;
+    cout << "Size of double is " << sizeof(double) << " bytes" << endl;
 
     return 0;
 }
