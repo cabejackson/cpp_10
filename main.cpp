fd9609561922b29058c9fcc99d8795bf5a4599ad
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-03 16:29:31 -0600

    Add intro to ASCII tables

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    //const
-    // - A compile time contratint that an object cannot be modified
-    //example:
-    const int i = 9;
-    i = 6;
 
+    //ASCII Table
+    //Ea. char is represented by a number
+
+    //getting a num from a char
+    cout << (int)'a' << endl;
+    cout << int('a') << endl;
+
+    cout << int('A') << endl;
+
+    //getting a char from a num
+    cout << char(65) << endl;
+
+    
     return 0;
 }
