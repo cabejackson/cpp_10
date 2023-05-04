886a45cd7c73814f27bc39fb43add7de035f50f4
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-04 11:59:03 -0600

    Add program for determining triangle type

--- main.cpp
+++ main.cpp
@@ -2,17 +2,33 @@
 using namespace std;
 
 int main() {
-    int num;
+    // int num;
 
-    cout << "Please enter a whole number: ";
-    cin >> num;
+    // cout << "Please enter a whole number: ";
+    // cin >> num;
 
-    if (num % 2 == 0) {
-        cout << "Your number is even" << endl;
-    }else {
-        cout << "Your number is odd" << endl; 
-    };
-    cout << "Thanks, bye!";
+    // if (num % 2 == 0) {
+    //     cout << "Your number is even" << endl;
+    // }else {
+    //     cout << "Your number is odd" << endl; 
+    // };
+    // cout << "Thanks, bye!";
+
+    //user enters side lengths of a triangle (a, b, c)
+    // Program should determine whether the triangle is equilateral, isosceles or scalene
+    double a, b, c;
+    cout << "Please enter 3 side lengths: ";
+    cin >> a >> b >> c;
+
+    if ( a == b && b == c)
+        cout << "Your triangle is equilateral!" << endl;
+    else {
+        if ( a != b && b != c && c != a)
+            cout << "Your triangle is scalene!" << endl;
+        else
+            cout << "Your triangle is isosceles!" << endl;
+    }
+
 
     return 0;
 }
