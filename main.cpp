b1ce5c3a1c990e015b634478d083eee306e56525
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-08 15:33:34 -0600

    Add unary operator (counters) example

--- main.cpp
+++ main.cpp
@@ -11,5 +11,15 @@ int main() {
     cout << 5.0 / 2.0 << endl; // double/float
     cout << 5 % 2 << endl; // modulo -- remainder
 
+    //unary operators
+    //++, --
+
+    int counter = 7;
+    counter++;
+    cout << counter << endl;
+    counter--;
+    cout << counter << endl;
+
+    
     return 0;
 }
