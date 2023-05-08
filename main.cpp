59437c083b11319a763d4d5aca14d50850bf515e
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-08 16:10:24 -0600

    Create program for swapping 2 variables without using a 3rd variable

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
@@ -6,15 +7,29 @@ int main() {
     //program for swapping values of two variables
     //using a third variable
 
+    // int a = 20;
+    // int b = 10;
+
+    // cout << "a = " << a << " and b = " << b << endl; // a = 20 and b = 10
+
+    // int temp = a; //temporary variable that holds the value of a
+    // a = b;
+    // b = temp;
+
+    // cout << "a = " << a << " and b = " << b << endl; // a = 10 and b = 20
+    
+    //program for swapping values of two variables
+    //without using a third variable
 
     int a = 20;
     int b = 10;
 
     cout << "a = " << a << " and b = " << b << endl; // a = 20 and b = 10
 
-    int temp = a; //temporary variable that holds the value of a
-    a = b;
-    b = temp;
+    a = a + b; //30
+    b = a - b; //20
+    a = abs(b - a); //10
+    //a = a - b //10 //another solution
 
     cout << "a = " << a << " and b = " << b << endl; // a = 10 and b = 20
 
