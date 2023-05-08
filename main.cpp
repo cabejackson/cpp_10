21e9e11d01a8a6dfeb15d87b963ee9df8e876edb
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-08 16:25:53 -0600

    Create a bmi calculator program using metric system

--- main.cpp
+++ main.cpp
@@ -2,11 +2,32 @@
 using namespace std;
 
 int main() {
-    //const
-    // - A compile time contratint that an object cannot be modified
-    //example:
-    const int i = 9;
-    i = 6;
+
+    //BMI Calculator
+    // weight (kg) / height * height (m)
+    // underweight < 18.5
+    // normal weight 18.5 - 24.9
+    // overweight >25
+
+    double bmi;
+    double weight;
+    double height;
+
+     cout << "Please enter your weight(in kg): ";
+     cin >> weight;
+     cout << "Please enter your height(in m): ";
+     cin >> height;
+
+     bmi = weight / (height * height);
+
+    if (bmi < 18.5) {
+        cout << "Underweight." << endl;
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        cout << "Normal weight." << endl;
+    } else {
+        cout << "Overweight." << endl;
+    }
+    cout << "Your bmi is " << bmi << endl;
 
     return 0;
 }
