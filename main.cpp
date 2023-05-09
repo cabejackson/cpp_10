a6d2ce080ce4ce946ec0d70073a84f9aa8f9df17
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-09 06:05:04 -0600

    Add % operation

--- main.cpp
+++ main.cpp
@@ -21,6 +21,15 @@ int main() {
         case '/':
             cout << num1 / num2 << endl;
             break;
+        case '%':
+            bool isNum1Int, isNum2Int;
+            isNum1Int = ((int)num1 == num1); //5 == 5.0
+            isNum2Int = ((int)num2 == num2);
+            if (isNum1Int && isNum2Int)
+                cout << (int)num1 % (int)num2 << endl;
+            else
+                cout << "invalid" << endl;
+            break;
         default:
             cout << "invalid operation" << endl;
             break; 
