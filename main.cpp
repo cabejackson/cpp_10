d40b6b3a43e8da1871ba649823c34747d698cdf8
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-09 06:37:27 -0600

    Create a program that checks number of days in a month

--- main.cpp
+++ main.cpp
@@ -2,11 +2,29 @@
 using namespace std;
 
 int main() {
-    //const
-    // - A compile time contratint that an object cannot be modified
-    //example:
-    const int i = 9;
-    i = 6;
+
+    //leap year rule:
+    // (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+    int year, month;
+
+    cout << "Please enter the year and month (ex: 2004 4): ";
+    cin >> year >> month;
+
+    switch(month) {
+        case 2: (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)? 
+            cout << "29 days" << endl : cout << "28 days" << endl; break;
+        case 4:
+        case 6:
+        case 9:
+        case 11: cout << "30 days month" << endl; break;
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12: cout << "31 days month" << endl; break;
+   }
 
     return 0;
 }
