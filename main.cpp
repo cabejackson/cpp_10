5c3b7f5f66f3287b2be540fc5ab2740bd368cdc5
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-09 07:48:09 -0600

    Create a program that counts digits of a number

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
@@ -7,12 +8,28 @@ int main() {
 
     // write out all the numbers between 100 - 500 that are divisible by 3 and 5
 
-    int counter = 100;
+    // int counter = 100;
 
-    while (counter <= 500 && counter >= 100){
-        if (counter % 3 == 0 && counter % 5 == 0)
-            cout << counter << " is divisible by 3 and 5" << endl;
-        counter++;
+    // while (counter <= 500 && counter >= 100){
+    //     if (counter % 3 == 0 && counter % 5 == 0)
+    //         cout << counter << " is divisible by 3 and 5" << endl;
+    //     counter++;
+    // };
+
+    //count digits of a number
+
+    int num, counter = 0;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num == 0)
+        cout << "You have entered 0." << endl;
+    else {
+        while( num > 0) {
+            num /= 10;
+            counter++;
+        }
+        cout << "number contains " << counter << " digits." << endl;
     }
 
     return 0;
