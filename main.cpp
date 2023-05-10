75e620576c8c4c01aca843ecceb462f17c535cc5
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-10 17:59:47 -0600

    Create a program that reverses numbers

--- main.cpp
+++ main.cpp
@@ -19,22 +19,38 @@ int main() {
     //count digits of a number
     //what about nums larger than int?
 
-    int num, counter = 0;
-    cout << "Enter a number: ";
+    // int num, counter = 0;
+    // cout << "Enter a number: ";
+    // cin >> num;
+
+    // if (num == 0)
+    //     cout << "You have entered 0." << endl;
+    // else {
+    //     if (num < 0) {
+    //         num *= -1; //get abs value of num
+    //     }
+    //     while( num > 0) {
+    //         num /= 10;
+    //         counter++;
+    //     }
+    //     cout << "number contains " << counter << " digits." << endl;
+    // }
+
+    //reversing number
+    //confusing
+
+    int num, reversedNum = 0, lastDigit;
+    cout << "Please enter a number: ";
     cin >> num;
 
-    if (num == 0)
-        cout << "You have entered 0." << endl;
-    else {
-        if (num < 0) {
-            num *= -1; //get abs value of num
-        }
-        while( num > 0) {
-            num /= 10;
-            counter++;
-        }
-        cout << "number contains " << counter << " digits." << endl;
+    while (num != 0) {
+        reversedNum *= 10;
+        lastDigit = num % 10;
+        reversedNum += lastDigit;
+        num /= 10;
     }
+
+    cout << "Reversed: " << reversedNum << endl;
 
     return 0;
 }
