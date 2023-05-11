62fdf1d319170c717ce2ea2026bc48c1285462b8
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-11 17:24:32 -0600

    Create a program that checks for prime numbers (using a function)

--- main.cpp
+++ main.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-
-    int num;
+bool isPrimeNumber(int num) {
     bool isPrime = true;
-
-    cout << "Enter a number: ";
-    cin >> num;
-
-    for ( int i = 2; i < num; i++){
+        for ( int i = 2; i < num; i++){
         if (num % i == 0) {
             isPrime = false;
             break;
         }
     }
+    return isPrime;
+}
+
+int main() {
+
+    int num;
+    bool isPrime = isPrimeNumber(num);
+
+    cout << "Enter a number: ";
+    cin >> num;
+
     if (isPrime)
         cout << num <<" is a prime number!" << endl;
     else
