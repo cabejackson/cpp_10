282ee03445ca5a397b6c2ac1862e894e6fa357de
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-11 16:50:44 -0600

    Add logic to reverse the triangle

--- main.cpp
+++ main.cpp
@@ -38,6 +38,14 @@ int main() {
         };
         cout << endl;
     }
+    cout << endl << endl;
+    //reverse the triangle
+    for (int i = length; i >= 1; i--){
+        for ( int j = 1; j <= i; j++) {
+            cout << setw(2) << symbol;
+        };
+        cout << endl;
+    }
 
     return 0;
 }
