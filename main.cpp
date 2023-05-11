631f75a34774104060db8a16c1e06ef34ab56446
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-11 16:46:21 -0600

    Create program that draws triangles

--- main.cpp
+++ main.cpp
@@ -4,20 +4,38 @@ using namespace std;
 
 int main() {
 
-    int height, width;
+    //shape: rectangles
+
+    // int height, width;
+    // char symbol;
+
+    // cout << "Height: ";
+    // cin >> height;
+    // cout << "Width: ";
+    // cin >> width;
+    // cout << "Enter a symbol: ";
+    // cin >> symbol;
+
+    // for (int h = 0; h < height; h++){
+    //     for( int w = 0; w < width; w++) {
+    //         cout << setw(3) << symbol ;
+    //     }
+    //     cout << endl;
+    // }
+
+    //shape: triangles
+    int length;
     char symbol;
 
-    cout << "Height: ";
-    cin >> height;
-    cout << "Width: ";
-    cin >> width;
+    cout << "Length: ";
+    cin >> length;
     cout << "Enter a symbol: ";
     cin >> symbol;
 
-    for (int h = 0; h < height; h++){
-        for( int w = 0; w < width; w++) {
-            cout << setw(3) << symbol ;
-        }
+    for (int i = 1; i <= length; i++){
+        for ( int j = 1; j <= i; j++) {
+            cout << setw(2) << symbol;
+        };
         cout << endl;
     }
 
