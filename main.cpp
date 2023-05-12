f49d8b920204717c37481171a1d09a308ba45ec9
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-12 17:35:36 -0600

    Setup generic example using function overloading at first

--- main.cpp
+++ main.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
 using namespace std;
 
+void Swap( int& a, int& b) { //passing variables by reference
+    int temp = a;
+    a = b;
+    b = temp;
+}
+//ex of function overloading
+void Swap( char& a, char& b) { 
+    char temp = a;
+    a = b;
+    b = temp;
+}
 int main() {
-    //const
-    // - A compile time contratint that an object cannot be modified
-    //example:
-    const int i = 9;
-    i = 6;
+
+    //generics - 
+        // using the same code w/ dif data types
+        //passing the type of params to a function
+
+        int a = 5, b = 7;
+
+        cout << a << " - " << b << endl;
+        Swap(a, b);
+        cout << a << " - " << b << endl;
+
+        char c = 'c', d = 'd';
+
+        cout << c << " - " << d << endl;
+        Swap(c, d);
+        cout << c << " - " << d << endl;
 
     return 0;
 }
