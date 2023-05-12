83a68138503fa3a8fcfd5e006a77a85e77819525
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-12 17:38:53 -0600

    Add generics example

--- main.cpp
+++ main.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 using namespace std;
 
-void Swap( int& a, int& b) { //passing variables by reference
-    int temp = a;
+//generic
+template<typename T>
+void Swap( T& a, T& b) { //passing variables by reference
+    T temp = a;
     a = b;
     b = temp;
 }
-//ex of function overloading
-void Swap( char& a, char& b) { 
-    char temp = a;
-    a = b;
-    b = temp;
-}
+// void Swap( int& a, int& b) { //passing variables by reference
+//     int temp = a;
+//     a = b;
+//     b = temp;
+// }
+// //ex of function overloading
+// void Swap( char& a, char& b) { 
+//     char temp = a;
+//     a = b;
+//     b = temp;
+// }
 int main() {
 
     //generics - 
@@ -21,13 +28,13 @@ int main() {
         int a = 5, b = 7;
 
         cout << a << " - " << b << endl;
-        Swap(a, b);
+        Swap<int>(a, b); 
         cout << a << " - " << b << endl;
 
         char c = 'c', d = 'd';
 
         cout << c << " - " << d << endl;
-        Swap(c, d);
+        Swap<char>(c, d);
         cout << c << " - " << d << endl;
 
     return 0;
