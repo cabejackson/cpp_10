2702f5960986122eafb6ebed45b58edecac77ab8
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-12 17:24:56 -0600

    Add fucntion overloading example

--- main.cpp
+++ main.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
 using namespace std;
 
-bool isPrimeNumber(int num) {
-    for (int i = 2; i < num; i++){
-        if (num % i == 0) {
-            return false;
-        }
-    }
-    return true;
-}
-
+//function overloading
+//if you have dif params/num of params/return types you dont need to rename each func a dif name
+int sum (int a, int b);
+double sum (double a, double b);
+float sum (float a, float b, float c);
 
 int main() {
 
-    for (int i = 1; i <= 100000; i++) {
-        bool isPrime = isPrimeNumber(i);
-        //TODO: Add a counter for how many prime numbers there are w/in the range
-        if (isPrime)
-            cout << i <<" is a prime number!" << endl;
-    }
+    cout << sum(4, 3) << endl;
+    cout << sum(4.4, 3.3) << endl;
+    cout << sum(4.4, 3.3, 2.2) << endl;
+
     return 0;
 }
+
+int sum (int a, int b) {
+    return a + b;
+};
+double sum (double a, double b) {
+    return a + b;
+};
+float sum (float a, float b, float c) {
+    return a + b + c;
+};
