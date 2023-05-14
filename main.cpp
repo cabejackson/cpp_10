9e9636964c12399c4238a1093d4283722d0326c7
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-13 20:11:44 -0600

    Encapsulate subscribe and unsubscribe functionality

--- main.cpp
+++ main.cpp
@@ -3,12 +3,13 @@
 using namespace std;
 
 class YoutubeChannel {
-public: //access modifier, but private by default
+private:
     string Name;
     string OwnerName;
     int SubscribersCount;
     list<string> PublishedVideoTitles;
 
+public: //access modifier, but private by default
     //constructor
     YoutubeChannel(string name, string ownerName) {
         Name = name;
@@ -27,18 +28,29 @@ public: //access modifier, but private by default
             cout << videoTitle << endl;
         }
     }
+    void Subscribe() {
+        SubscribersCount++;
+    };
+    void Unsubscribe() {
+        SubscribersCount--;
+    };
 };
 
 int main() {
 
     YoutubeChannel ytChannel("Cur1ousCod3r", "Cabe");
-    ytChannel.PublishedVideoTitles.push_back("C++ for beginners");
-    ytChannel.PublishedVideoTitles.push_back("HTML & CSS for beginners");
-    ytChannel.PublishedVideoTitles.push_back("C++ OOP");
+    // ytChannel.PublishedVideoTitles.push_back("C++ for beginners");
+    // ytChannel.PublishedVideoTitles.push_back("HTML & CSS for beginners");
+    // ytChannel.PublishedVideoTitles.push_back("C++ OOP");
     YoutubeChannel ytChannel2("CodeBeauty", "Saldina");
     //this line below doesn't work in my editor
     // ytChannel.PublishedVideoTitles = {"C++ for beginnners Video 1", "HTML & CSS Video 1", "C++ OOP Video 1"};
 
+    
+    ytChannel.Subscribe();
+    ytChannel.Subscribe();
+    ytChannel.Subscribe();
+    ytChannel.Unsubscribe();
     ytChannel.GetInfo();
     ytChannel2.GetInfo();
 
