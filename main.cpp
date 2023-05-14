febe35edeae6446b05692fbfa475667a985267a8
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-13 19:41:58 -0600

    Create a YoutubeChannel class and a ytChannel object/instance

--- main.cpp
+++ main.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
+#include <list>
 using namespace std;
 
+class YoutubeChannel {
+public: //access modifier, but private by default
+    string Name;
+    string OwnerName;
+    int SubscribersCount;
+    list<string> PublishedVideoTitles;
+};
+
 int main() {
-    //const
-    // - A compile time contratint that an object cannot be modified
-    //example:
-    const int i = 9;
-    i = 6;
+
+    YoutubeChannel ytChannel;
+    //assign values to Name, OwnerName, etc. properties
+    ytChannel.Name = "Cur1ousCod3r";
+    ytChannel.OwnerName = "Cabe";
+    ytChannel.SubscribersCount = 1800;
+    //this line below doesn't work in my editor
+    // ytChannel.PublishedVideoTitles = {"C++ for beginnners Video 1", "HTML & CSS Video 1", "C++ OOP Video 1"};
+
+    cout << "Name: " << ytChannel.Name << endl; 
+    cout << "Owner Name: " << ytChannel.OwnerName << endl; 
+    cout << "Subscribers Count: " << ytChannel.SubscribersCount << endl; 
+    // cout << "Videos: " << endl;
+    //these lines below don't work in my editor
+    // for (string videoTitle: ytChannel.PublishedVideoTitles) {
+    //     cout << videoTitle << endl;
+    // }
 
     return 0;
 }
