0f12a40a347f32fb8e63e07760989c4b580581df
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-13 19:58:40 -0600

    Delete excess/non-dry code

--- main.cpp
+++ main.cpp
@@ -20,31 +20,21 @@ public: //access modifier, but private by default
 int main() {
 
     YoutubeChannel ytChannel("Cur1ousCod3r", "Cabe");
-    // YoutubeChannel ytChannel;
-    //assign values to Name, OwnerName, etc. properties
-    // ytChannel.Name = "Cur1ousCod3r";
-    // ytChannel.OwnerName = "Cabe";
-    // ytChannel.SubscribersCount = 1800;
+    ytChannel.PublishedVideoTitles.push_back("C++ for beginners");
+    ytChannel.PublishedVideoTitles.push_back("HTML & CSS for beginners");
+    ytChannel.PublishedVideoTitles.push_back("C++ OOP");
+    YoutubeChannel ytChannel2("CodeBeauty", "Saldina");
     //this line below doesn't work in my editor
     // ytChannel.PublishedVideoTitles = {"C++ for beginnners Video 1", "HTML & CSS Video 1", "C++ OOP Video 1"};
 
     cout << "Name: " << ytChannel.Name << endl; 
     cout << "Owner Name: " << ytChannel.OwnerName << endl; 
     cout << "Subscribers Count: " << ytChannel.SubscribersCount << endl; 
-    // cout << "Videos: " << endl;
-    //these lines below don't work in my editor
-    // for (string videoTitle: ytChannel.PublishedVideoTitles) {
-    //     cout << videoTitle << endl;
-    // }
-
-    //setup for why constructors and class methods are useful
-    //not very DRY
-    YoutubeChannel ytChannel2("CodeBeauty", "Saldina");
-    // YoutubeChannel ytChannel2;
-    // //assign values to Name, OwnerName, etc. properties
-    // ytChannel2.Name = "CodeBeauty";
-    // ytChannel2.OwnerName = "Saldina";
-    // ytChannel2.SubscribersCount = 2000;
+    cout << "Videos: " << endl;
+    //these lines below trigger a warning in my editor
+    for (string videoTitle : ytChannel.PublishedVideoTitles) {
+        cout << videoTitle << endl;
+    }
 
     cout << "Name: " << ytChannel2.Name << endl; 
     cout << "Owner Name: " << ytChannel2.OwnerName << endl; 
