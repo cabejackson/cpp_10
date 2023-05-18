c998e55dcdc9763e4f51f420ee15f92af0dbcbb7
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-17 19:24:42 -0600

    Add pointers and dereferenced pointer

--- main.cpp
+++ main.cpp
@@ -6,7 +6,13 @@ int main() {
     int n = 5; //a variable
     cout << "Variable: " << n << endl;
     //how do we check the address of n
-    cout << "Address: " << &n << endl;
+    cout << "Address of 'n' variable: " << &n << endl;
+
+    int* ptr = &n;
+    cout << "Address of 'n' variable: " << ptr << endl;
+
+    //dereferencing pointers
+    cout << "Variable: " << *ptr << endl; //outputs value at that location
 
 
     return 0;
