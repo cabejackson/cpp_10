1f8973c632d36f4795e0be2ff672de55e57223d8
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-18 07:17:49 -0600

    Write out a variable using a func that receives a pointer

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
 
+//write out a variable using a func that receives a pointer
+void printNumber(int* numberPtr) {
+    cout << *numberPtr << endl;
+};
+
 int main() {
+
+    int num = 5;
+    printNumber(&num);
+
 
     return 0;
 }
