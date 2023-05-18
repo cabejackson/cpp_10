81000c0053b0e1173e6f27cb811a11f821585d8a
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-18 07:28:00 -0600

    Add void pointer example

--- main.cpp
+++ main.cpp
@@ -5,11 +5,24 @@ using namespace std;
 void printNumber(int* numberPtr) {
     cout << *numberPtr << endl;
 };
+void printLetter(char* charPtr) {
+    cout << *charPtr << endl;
+};
+void print(void* ptr, char type) {
+    switch(type){
+        case 'i': cout << *((int*)ptr) << endl; break;
+        case 'c': cout << *((char*)ptr) << endl; break;
+    };
+};
 
 int main() {
 
     int num = 5;
-    printNumber(&num);
+    char letter = 'a';
+    // printNumber(&num);
+    // printLetter(&letter);
+    print(&num, 'i');
+    print(&letter, 'c');
 
 
     return 0;
