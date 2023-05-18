e008603cb4a744171c6d51347f87e899328ff638
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-17 19:34:51 -0600

    Add start of pointers2.0

--- main.cpp
+++ main.cpp
@@ -2,31 +2,6 @@
 using namespace std;
 
 int main() {
-    //note/questions:
-    // pointers and variables need to be of the same type
-    // why would you want to dereference a pointer?
-
-
-    int n = 5; //a variable
-    cout << "Variable: " << n << endl;
-    //how do we check the address of n
-    cout << "Address of 'n' variable: " << &n << endl;
-
-    int* ptr = &n;
-    cout << "Address of 'n' variable: " << ptr << endl;
-
-    //dereferencing pointers
-    cout << "Variable: " << *ptr << endl; //outputs value at that location
-    //reassigning value at pointer location
-    *ptr = 10;
-    cout << "New Value: " << *ptr << endl; //outputs value at that location
-    cout << "Variable: " << n << endl;
-
-    int v;
-    int* ptr2 = &v;
-    *ptr2 = 7;
-    cout << "v= " << v << endl;
-
 
     return 0;
 }
