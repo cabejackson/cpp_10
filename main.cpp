5263245f0c8335a1f15e3b6139b85ad2f833a2ef
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-19 18:43:15 -0600

    Add getMax() function

--- main.cpp
+++ main.cpp
@@ -9,7 +9,15 @@ int getMin(int numbers[], int size) {
             min = numbers[i];
     }
     return min;
+};
 
+int getMax(int numbers[], int size) {
+    int max = numbers[0];
+    for (int i = 1; i <= size;  i++){
+        if(numbers[i] > max)
+            max = numbers[i];
+    }
+    return max;
 };
 
 int main() {
@@ -36,6 +44,7 @@ int main() {
     int numbers[5] = {5, 4, -2, 29, 6};
 
     cout << "Min is " << getMin(numbers, 5) << endl;
+    cout << "Max is " << getMax(numbers, 5) << endl;
 
 
 
