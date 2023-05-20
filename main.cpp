f128159ec90a9734dc93828f22cca51ee674c823
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-19 18:41:54 -0600

    Add getMin() function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+//using pointers to return multiple values from a function
+int getMin(int numbers[], int size) {
+    int min = numbers[0];
+    for (int i = 1; i <= size;  i++){
+        if(numbers[i] < min)
+            min = numbers[i];
+    }
+    return min;
+
+};
 
 int main() {
     // int luckyNumbers[5] = { 2, 3, 5, 7, 9 };
@@ -12,16 +22,22 @@ int main() {
     // cout << "Address of luckyNumbers[2]: " << &luckyNumbers[2] << endl;
 
     //how to enter values via an array
-    int luckyNumbers[5]; 
+    // int luckyNumbers[5];
 
-    for(int i = 0; i <= 4; i++) {
-        cout << "Number: ";
-        cin >> luckyNumbers[i];
-    }
+    // for(int i = 0; i <= 4; i++) {
+    //     cout << "Number: ";
+    //     cin >> luckyNumbers[i];
+    // }
 
-    for (int i = 0; i <= 4; i++) {
-        cout << *(luckyNumbers+i) << " ";
-    }
+    // for (int i = 0; i <= 4; i++) {
+    //     cout << *(luckyNumbers+i) << " ";
+    // }
+
+    int numbers[5] = {5, 4, -2, 29, 6};
+
+    cout << "Min is " << getMin(numbers, 5) << endl;
+
+
 
     return 0;
 }
