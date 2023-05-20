59bb56d316b95c9e83433ce150343bcd02b7ab57
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-19 18:32:14 -0600

    Use pointers and arrays example

--- main.cpp
+++ main.cpp
@@ -1,29 +1,27 @@
 #include <iostream>
 using namespace std;
 
-//write out a variable using a func that receives a pointer
-void printNumber(int* numberPtr) {
-    cout << *numberPtr << endl;
-};
-void printLetter(char* charPtr) {
-    cout << *charPtr << endl;
-};
-void print(void* ptr, char type) {
-    switch(type){
-        case 'i': cout << *((int*)ptr) << endl; break;
-        case 'c': cout << *((char*)ptr) << endl; break;
-    };
-};
 
 int main() {
+    // int luckyNumbers[5] = { 2, 3, 5, 7, 9 };
 
-    int num = 5;
-    char letter = 'a';
-    // printNumber(&num);
-    // printLetter(&letter);
-    print(&num, 'i');
-    print(&letter, 'c');
+    // cout << luckyNumbers << endl; // the address of the first element of the array is printed
+    // cout << &luckyNumbers[0] << endl;
+    // cout << "Value: "<< luckyNumbers[2] << endl;
+    // cout << "Value: "<< *(luckyNumbers + 2) << endl; //dereferencing
+    // cout << "Address of luckyNumbers[2]: " << &luckyNumbers[2] << endl;
 
+    //how to enter values via an array
+    int luckyNumbers[5]; 
+
+    for(int i = 0; i <= 4; i++) {
+        cout << "Number: ";
+        cin >> luckyNumbers[i];
+    }
+
+    for (int i = 0; i <= 4; i++) {
+        cout << *(luckyNumbers+i) << " ";
+    }
 
     return 0;
 }
