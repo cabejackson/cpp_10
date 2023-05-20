230ac76861cac95f2d9e5e67b364074fc07491bb
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-19 18:54:09 -0600

    Add getMinAndMax() func which uses pointers to return multiple values from a function

--- main.cpp
+++ main.cpp
@@ -20,6 +20,16 @@ int getMax(int numbers[], int size) {
     return max;
 };
 
+void getMinAndMax(int numbers[], int size, int* min, int* max ) {
+    for (int i = 1; i <= size;  i++){
+        if(numbers[i] > *max)
+            *max = numbers[i];
+        if(numbers[i] < *min)
+            *min = numbers[i];
+    };
+
+}
+
 int main() {
     // int luckyNumbers[5] = { 2, 3, 5, 7, 9 };
 
@@ -43,8 +53,17 @@ int main() {
 
     int numbers[5] = {5, 4, -2, 29, 6};
 
-    cout << "Min is " << getMin(numbers, 5) << endl;
-    cout << "Max is " << getMax(numbers, 5) << endl;
+    // cout << "Min is " << getMin(numbers, 5) << endl;
+    // cout << "Max is " << getMax(numbers, 5) << endl;
+
+    int min = numbers[0];
+    int max = numbers[0];
+
+    getMinAndMax(numbers, 5, &min, &max); 
+    //passing a parameter using a reference
+    //passing an address of a variable instead of teh variable itself
+    cout << "Min is " << min << endl;
+    cout << "Max is " << max << endl;
 
 
 
